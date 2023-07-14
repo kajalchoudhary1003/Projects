aae8c901d756acdf95cf788d71de723e6ee022be
Kajal Choudhary <108188712+kajalchoudhary1003@users.noreply.github.com>
2023-07-15 01:42:16 +0530

    __

--- todo-list/src/components/todo.js
+++ todo-list/src/components/todo.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import './style.css'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faPlus} from '@fortawesome/free-solid-svg-icons'
+import {faPlus}, {faPenToSquare} from '@fortawesome/free-solid-svg-icons';
 
 const Todo = () => {
   return (
     <>
-      <div className="main-div">
+      <div className="main-div w-full h-screen flex justify-center items-center">
         <div className="child-div">
             <figure>
                 <img src="./images/todo.png" alt="todLogo" />
@@ -16,6 +15,21 @@ const Todo = () => {
                 <input type="text" name="" id="" placeholder='✍️ Add Your Items Here'
                 className='form-control' />
                 <FontAwesomeIcon icon={faPlus} size="2xs" style={{color: "#000000",}} />
+            </div>
+            {/* items to be shown */}
+            <div className="showItems">
+              <div className="eachItem">
+                <h3>Apple</h3>
+                <div className="todo-btn">
+                <FontAwesomeIcon icon={faPenToSquare} size="xs" style={{color: "#000000",}} />
+                </div>
+              </div>
+            </div>
+            {/* remove button */}
+            <div className="showItems">
+              <button className="remove">
+                Remove All
+              </button>
             </div>
         </div>
       </div>
